a1f5330342f51680f1f5f70a07a20c746a8d51e6
Josh Koter <20446632+Koda-Pig@users.noreply.github.com>
2024-08-09 15:51:33 +0200

    add colors

--- src/shuffle.ts
+++ src/shuffle.ts
@@ -12,7 +12,8 @@ export function shuffleTitleColour(
     "#f6e96b, #bedc74, #a2ca71, #387f39, #f6e96b",
     "#7C00FE, #F9E400, #FFAF00, #F5004F, #7C00FE",
     "#6C946F, #FFD35A, #FFA823, #DC0083, #6C946F",
-    "#77E4C8, #36C2CE, #478CCF, #4535C1, #77E4C8"
+    "#77E4C8, #36C2CE, #478CCF, #4535C1, #77E4C8",
+    "#D6EFD8, #80AF81, #508D4E, #1A5319, #D6EFD8"
   ];
 
   cardElement.addEventListener("click", () => {
